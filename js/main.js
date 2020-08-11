00dcd24f20d8f0bb1cbeb7433b9041e8ccb656a5
ranariad <48047510+ranariad@users.noreply.github.com>
2020-08-11 10:01:56 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
+(function($) {
+  "use strict"; // Start of use strict
 
  // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function() {
     $('.navbar-collapse').collapse('hide');
   });
+ 
+})(jQuery);
